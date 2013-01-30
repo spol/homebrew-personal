d11411a6eb205f3306340dc80b7391a9258757b3
Seb Pollard <seb@spolster.co.uk>
2013-01-30 14:11:08 +0000

    Initial commit

---
+++ Formula/denonctl.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Denonctl < Formula
+  homepage 'https://github.com/spol/DenonCtl'
+  url 'https://github.com/spol/DenonCtl/archive/master.zip'
+  sha1 '060e39ebc1cc5b4f3d9ab827be750a631eff045e'
+  head 'https://github.com/spol/DenonCtl.git'
+
+
+end
---
+++ README.md
@@ -0,0 +1,1 @@
+A central repo for various scripts.
