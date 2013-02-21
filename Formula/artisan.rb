098f9e3e5aeaa7e10396db6b1d07ca0e743c367c
Seb Pollard <seb@spolster.co.uk>
2013-02-21 21:20:12 +0000

    Fixed artisan sha1.

--- Formula/artisan.rb
+++ Formula/artisan.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Artisan < Formula
   homepage 'https://github.com/spol/ArtisanCli'
   url 'https://github.com/spol/ArtisanCli/archive/v0.1.1.zip'
-  sha1 '8c13b1406dbe4c3f6cb88e7a7287965e5033111e'
+  sha1 '10449f9d04791557e2ca20c9ffa957647c314359'
   head 'https://github.com/spol/ArtisanCli.git'
   version '0.1.1'
 
