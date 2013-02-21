a38932c6b11da97d097ec65889093481f7479c5f
Seb Pollard <seb@spolster.co.uk>
2013-02-21 21:18:58 +0000

    Updated formula for artisan v0.1.1

--- Formula/artisan.rb
+++ Formula/artisan.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Artisan < Formula
   homepage 'https://github.com/spol/ArtisanCli'
-  url 'https://github.com/spol/ArtisanCli/archive/v0.1.0.zip'
-  sha1 '3347a196bb0033432ea9383c2802c869d32eccff'
+  url 'https://github.com/spol/ArtisanCli/archive/v0.1.1.zip'
+  sha1 '8c13b1406dbe4c3f6cb88e7a7287965e5033111e'
   head 'https://github.com/spol/ArtisanCli.git'
-  version '0.1.0'
+  version '0.1.1'
 
   def install
     bin.install('artisan')
